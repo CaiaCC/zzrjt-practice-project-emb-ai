1b904d73586d78433403d402bb324db55cd464b4
Caia Chuang <caiachuang@gmail.com>
2023-12-07 14:31:14 -0800

    fix sentiment_analyzer

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -5,8 +5,9 @@ def sentiment_analyzer(text_to_analyse):
     URL = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     INPUT_JSON = { "raw_document": { "text": text_to_analyse } }
     HEADER = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+
     response = requests.post(URL, json = INPUT_JSON, headers=HEADER)
-    formatted_response = json.load(response)
+    formatted_response = json.loads(response.text)
 
     return {
         'label': formatted_response['documentSentiment']['label'],
